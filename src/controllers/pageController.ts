dcb59a52dee2e84e98d85c0fa0ef015cf5c05e66
Kaian Vasconcelos <kaianvasconcelos@gmail.com>
2023-09-23 00:16:43 -0300

    final commit 1.0

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,42 +1,53 @@
 import { Request, Response } from "express";
 import { createMenuObject } from "../helpers/createMenuObject";
+import { pets } from "../models/pets";
 
 export const home = (req: Request, res: Response) => {
+    let list = pets.getAll();
+
     res.render("pages/page", {
         menu: createMenuObject("all"),
         banner: {
             title: "Todos os animais",
             background: "allanimals.jpg",
         },
+        list,
     });
 };
 
 export const dogs = (req: Request, res: Response) => {
+    let list = pets.getFromType("dog");
+
     res.render("pages/page", {
         menu: createMenuObject("dog"),
         banner: {
             title: "Cachorros",
             background: "banner_dog.jpg",
         },
+        list,
     });
 };
 
 export const cats = (req: Request, res: Response) => {
+    let list = pets.getFromType("cat");
     res.render("pages/page", {
         menu: createMenuObject("cat"),
         banner: {
             title: "Gatos",
             background: "banner_cat.jpg",
         },
+        list,
     });
 };
 
 export const fishes = (req: Request, res: Response) => {
+    let list = pets.getFromType("fish");
     res.render("pages/page", {
         menu: createMenuObject("fish"),
         banner: {
             title: "Peixes",
             background: "banner_fish.jpg",
         },
+        list,
     });
 };
--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,3 +1,19 @@
 import { Request, Response } from "express";
+import { createMenuObject } from "../helpers/createMenuObject";
+import { pets } from "../models/pets";
 
-export const search = (req: Request, res: Response) => {};
+export const search = (req: Request, res: Response) => {
+    if (!req.query.q) {
+        res.redirect("/");
+        return;
+    }
+
+    let query: string = req.query.q as string;
+    let list = pets.getFromName(query);
+
+    res.render("pages/page", {
+        menu: createMenuObject(""),
+        list,
+        query,
+    });
+};
